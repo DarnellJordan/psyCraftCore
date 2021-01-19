358bfa91464cfa51e1384ab9ccb92f0ab18d7355
psyGamer <manuel.preibisch@gmail.com>
2021-01-19 20:55:33 +0100

    Initial Commit: Basic structure

---
+++ src/main/java/de/psyCraft/api/game/Game.java
@@ -0,0 +1,15 @@
+package de.psyCraft.api.game;
+
+import org.bukkit.Material;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+
+	public static List<Game> games = new ArrayList<>();
+	
+	public Game(String name, String description, Material icon) {
+	
+	}
+}
---
+++ src/main/java/de/psyCraft/core/LobbyManager.java
@@ -0,0 +1,5 @@
+package de.psyCraft.core;
+
+public class LobbyManager {
+
+}
